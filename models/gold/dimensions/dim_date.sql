2c1d9ba95f2aa9010e23f047630637d142cb9a44
Pablo Jiménez Sánchez <pabloJimenez910.alu@stemgranada.com>
2026-03-18 17:42:14 +0000

    Generacion de dimension date de forma dinamica

--- models/gold/dimensions/dim_date.sql
+++ models/gold/dimensions/dim_date.sql
@@ -1,12 +1,21 @@
--- Nota: Puedes usar dbt_utils.date_spine si lo tienes instalado
+-- Este bloque genera fechas desde 2024 hasta 2026 sin usar refs externos
+WITH date_series AS (
+    SELECT 
+        generate_series(
+            '2024-01-01'::date, 
+            '2026-12-31'::date, 
+            '1 day'::interval
+        )::date AS date_day
+)
 SELECT
     date_day,
     EXTRACT(YEAR FROM date_day) AS year,
     EXTRACT(MONTH FROM date_day) AS month,
+    EXTRACT(DAY FROM date_day) AS day,
     EXTRACT(DAYOFWEEK FROM date_day) AS day_of_week,
-    -- Regla: Lunes(1) a Sábado(6) = Abierto (1), Domingo(0) = Cerrado (0)
+    -- Lunes(1) a Sábado(6) Abierto, Domingo(0) Cerrado
     CASE 
         WHEN EXTRACT(DAYOFWEEK FROM date_day) = 0 THEN 0 
         ELSE 1 
     END AS is_store_open_day
-FROM {{ ref('stg_dates') }}
+FROM date_series
